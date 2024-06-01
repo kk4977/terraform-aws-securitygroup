4c1a1101a88d801b3795a89ffa30cf92f1f2b486
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-06-01 21:53:20 +0530

    Code Updated  by sivakumar

--- variables.tf
+++ variables.tf
@@ -1,47 +1,45 @@
-
 variable "project_name" {
-
   type = string
-
 }
+
 variable "environment" {
   type = string
-
 }
+
 variable "sg_name" {
   type = string
+}
 
+variable "sg_description" {
+  type = string
 }
 
 variable "vpc_id" {
-
   type = string
 }
 
 variable "common_tags" {
-  type    = map 
-  default = {}
-
+  type = map
 }
+
+variable "sg_tags" {
+  type = map
+  default = {}
+}
+
 variable "outbound_rules" {
-  type = list 
+  type = list
   default = [
     {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1" # all protocols
-      cidr_blocks = ["0.0.0.0/0"]
+        from_port = 0
+        to_port = 0
+        protocol = "-1" 
+        cidr_blocks = ["0.0.0.0/0"]
     }
   ]
 }
-variable "inbound_rules" {
-  type    = list 
+
+variable "ingress_rules" {
+  type = list
   default = []
-}
-variable "sg_description" {
-  type = string
-}
-variable "sg_tags" {
-  type    = map  
-  default = {}
 }
